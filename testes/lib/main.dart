17d6e80d82ca129a67c2fb8170269e8ca264da15
Josimar <contat.js@gmail.com>
2021-10-30 16:53:32 -0300

    Chamando eventos na main

--- testes/lib/main.dart
+++ testes/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:testes/pages/bloc/home_bloc.dart';
+import 'package:testes/pages/bloc/home_state.dart';
 import 'package:testes/pages/home_page.dart';
 
 void main() {
@@ -15,7 +18,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: BlocProvider<HomeBloc>(
+        create: (BuildContext context) => HomeBloc(HomeLoadingState()),
+        child: const HomePage(),
+      ),
     );
   }
 }
--- testes/lib/pages/bloc/home_bloc.dart
+++ testes/lib/pages/bloc/home_bloc.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:testes/bloc/home_event.dart';
-import 'package:testes/bloc/home_state.dart';
+import 'package:testes/pages/bloc/home_event.dart';
+import 'package:testes/pages/bloc/home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState>{
   HomeBloc(HomeState initialState) : super(HomeLoadingState());
