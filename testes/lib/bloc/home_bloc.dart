88fa83523f53c9855490ef996debf6e695cfbc1e
Josimar <contat.js@gmail.com>
2021-10-30 16:45:03 -0300

    Criando arquivos para gerenciar os eventos

---
+++ testes/lib/bloc/home_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:testes/bloc/home_event.dart';
+import 'package:testes/bloc/home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState>{
+  HomeBloc(HomeState initialState) : super(HomeLoadingState());
+
+  @override
+  Stream<HomeState> mapEventToState(HomeEvent event) async* {
+    throw UnimplementedError();
+  }
+}
